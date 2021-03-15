fb2fd204352b565646725bae2980762088f2d7d9
foad alhsnat <foadal0548@gmail.com>
2021-03-15 12:36:55 +0200

    6.1

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,33 @@
 import '../Card/Card.css'
 import faker from 'faker'
+import React, { Component } from 'react'
 
-const Card = (props) => {
-  return <div className="container">
-    <img src={props.src} />
-    <h1>{faker.lorem.words()}</h1>
-    <p>{faker.lorem.words()}</p>
-    <div>
-      <a href="/">share</a>
-      <a href="/">explor</a>
+// const Card = (props) => {
+//   return <div className="container">
+//     <img src={props.src} />
+//     <h1>{faker.lorem.words()}</h1>
+//     <p>{faker.lorem.words()}</p>
+//     <div>
+//       <a href="/">share</a>
+//       <a href="/">explor</a>
+//     </div>
+//   </div>
+// }
+
+class Card extends Component {
+  render() {
+    return <div className="container">
+      <img src={this.props.src} />
+      <h1>{faker.lorem.words()}</h1>
+      <p>{faker.lorem.words()}</p>
+      <div>
+        <a href="/">share</a>
+        <a href="/">explor</a>
+      </div>
     </div>
-  </div>
+  }
+
 }
+
 
 export default Card
