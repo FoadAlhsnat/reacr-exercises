736b6b4702fa16fba41fb4dabf9b51cb46d0374b
foad alhsnat <foadal0548@gmail.com>
2021-03-16 13:20:35 +0200

    8.1_update

--- src/components/LifeCycle/LifeCycle.jsx
+++ src/components/LifeCycle/LifeCycle.jsx
@@ -1,21 +1,34 @@
 import React, { Component } from 'react'
 
 export default class LifeCycle extends Component {
-  constructor(){
+  constructor() {
     super()
-    this.state={favoriteColor:'blue'}
+    this.state = { favoriteColor: 'blue' }
   }
 
-  componentDidMount= ()=>{
-    setTimeout(()=>this.setState({favoriteColor:'red'}),1000)
-    
+  componentDidMount = () => {
+    const div = document.getElementById('red')
+    // setTimeout(() =>
+    //   this.setState({ favoriteColor: 'red' }
+    //   ), 1000)
+
+      setTimeout(()=>{
+        this.setState({ favoriteColor: 'red' })
+        div.innerText=`the updated favorite color is${this.state.favoriteColor}`
+      },1000)
   }
 
   render() {
     return (
       <div>
         <h1>My favorite color is {this.state.favoriteColor}</h1>
+        <div>
+          <h1 id="red"></h1>
+
+        </div>
       </div>
     )
+
+
   }
 }
